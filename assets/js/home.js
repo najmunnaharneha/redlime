a79924ff911eaadcf1d2e55dd59dc95c548021cd
Neha <najmun.nahar.nehaa@gmail.com>
2023-01-05 05:15:51 +0600

    Icon Area

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,31 @@
-$(document).ready(function () {}); // Document.ready ends
+$(document).ready(function () {
+   //Fleet area starts
+   $(".fleet-contents").slick({
+      centerMode: true,
+      centerPadding: "200",
+      slidesToShow: 3,
+      prevArrow: true,
+      nextArrow: true,
+      responsive: [
+         {
+            breakpoint: 768,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: "40px",
+               slidesToShow: 3,
+            },
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: "40px",
+               slidesToShow: 1,
+            },
+         },
+      ],
+   });
+   //Fleet area ends
+}); // Document.ready ends
